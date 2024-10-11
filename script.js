2c0091981a80d80d67e9c71165bffa63e0d2083e
ktorres63 <ktorresaro@unsa.edu.pe>
2024-10-11 16:26:27 -0500

    Add functionality to buttons

--- script.js
+++ script.js
@@ -43,14 +43,14 @@ addBookForm.addEventListener("submit", (event) => {
   bookDialog.close();
 });
 
-// Función para mostrar los libros en un grid
+//Shows all books
 function displayBooks() {
   const libraryGrid = document.getElementById("libraryGrid");
 
   libraryGrid.innerHTML = "";
 
   // Añadir cada libro al grid
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     const titleDiv = document.createElement("div");
     titleDiv.textContent = book.title;
     titleDiv.classList.add("grid-item");
@@ -69,40 +69,73 @@ function displayBooks() {
 
     //Add buttons
     const btnsDiv = document.createElement("div");
-    btnsDiv.classList.add("btnsContainer")
+    btnsDiv.classList.add("btnsContainer");
     const isReadBtn = document.createElement("button");
     const deleteBtn = document.createElement("button");
 
-    const iconDelete = document.createElement("img")
-    iconDelete.src = "./imgs/icon_delete.svg"
-    iconDelete.alt = "delete"
+    isReadBtn.classList.add("btnRead");
+    deleteBtn.classList.add("btnDelete");
 
-    const iconComplete = document.createElement("img")
-    iconComplete.src = "./imgs/icon_completed.svg"
-    iconComplete.alt = "complete"
+    deleteBtn.dataset.indexNumber = index
+    isReadBtn.dataset.indexNumber = index
 
-    isReadBtn.prepend(iconComplete)
-    deleteBtn.prepend(iconDelete)
 
+    const iconDelete = document.createElement("img");
+    iconDelete.src = "./imgs/icon_delete.svg";
+    iconDelete.alt = "delete";
+
+    const iconComplete = document.createElement("img");
+    iconComplete.src = "./imgs/icon_completed.svg";
+    iconComplete.alt = "complete";
+
+    isReadBtn.prepend(iconComplete);
+    deleteBtn.prepend(iconDelete);
 
     const actionsDiv = document.createElement("div");
-    // actionsDiv.textContent = "delete - isRead"
     actionsDiv.classList.add("grid-item");
+
     btnsDiv.appendChild(isReadBtn);
     btnsDiv.appendChild(deleteBtn);
 
     actionsDiv.appendChild(btnsDiv);
-    // Añadir los elementos al grid
+
+    // Add elements to grid
     libraryGrid.appendChild(titleDiv);
     libraryGrid.appendChild(authorDiv);
     libraryGrid.appendChild(pagesDiv);
     libraryGrid.appendChild(isReadDiv);
     libraryGrid.appendChild(actionsDiv);
   });
-}
-console.log(myLibrary)
-// Mostrar los libros iniciales (si hubiera alguno)
-displayBooks();
 
-const newBookT = new Book("title", "author", 11, false);
+  const deleteButtons = document.querySelectorAll(".btnDelete");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = this.getAttribute("data-index-number");
+      console.log(`index: ${index}`)
+      removeBookFromLibrary(index);
+    });
+  });
+
+  const readButtons = document.querySelectorAll(".btnRead");
+  readButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = this.getAttribute("data-index-number");
+      console.log(`index: ${index}`)
+      changeStateBook(index);
+    });
+  });
+}
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1); 
+  displayBooks(); 
+}
+
+function changeStateBook(index) {
+  const book = myLibrary[index];
+  book.isRead = !book.isRead
+  console.log(`object:  ${book.isRead}`)
+  displayBooks(); 
+}
+
+const newBookT = new Book('1984', 'George Orwell', 328, false);
 addBookToLibrary(newBookT);
